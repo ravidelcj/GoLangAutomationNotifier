779b3298cc7c5a7103f3cf110f6c9f16738a1ced
ravidelcj <ravi.delcj@gmail.com>
2016-10-30 18:09:49 +0530

    main.go few changes

--- src/github.com/ravidelcj/main.go
+++ src/github.com/ravidelcj/main.go
@@ -3,18 +3,16 @@ package main
 import (
 
   "fmt"
-  "database/sql"
-	_ "github.com/go-sql-driver/mysql"
   "github.com/ravidelcj/scraper"
   "github.com/ravidelcj/database"
 )
 
 func main() {
 
-      Database.initDatabase()
-      go ScrapeNotice("http://ggsipuresults.nic.in/ipu/examnotice/examnoticemain.htm",db)
-      go ScrapeResults("http://ggsipuresults.nic.in/ipu/results/resultsmain.htm", db)
-      go ScrapeDatesheet("http://ggsipuresults.nic.in/ipu/datesheet/datesheetmain.htm", db)
+      database.InitDatabase()
+      go scraper.ScrapeNotice("http://ggsipuresults.nic.in/ipu/examnotice/examnoticemain.htm")
+      go scraper.ScrapeResults("http://ggsipuresults.nic.in/ipu/results/resultsmain.htm")
+      go scraper.ScrapeDatesheet("http://ggsipuresults.nic.in/ipu/datesheet/datesheetmain.htm")
 
       var endInput string
       fmt.Scanln(&endInput)
